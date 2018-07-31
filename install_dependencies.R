1d62c9435ef63c5361a8cb532c7b1f1c3419eec0
Andres Felipe López-Lopera <anfelopera@users.noreply.github.com>
2018-07-31 17:01:41 +0200

    First public version of the lineqGPR package

---
+++ install_dependencies.R
@@ -0,0 +1,7 @@
+#### run to install all the dependencies of the package lineqGPR ####
+# list of dependencies
+listPackages <- c("nloptr", "broom", "tmg", "mvtnorm","MASS", "quadprog",
+                  "Matrix", "restrictedMVN", "TruncatedNormal", "ggplot2",
+                  "viridis")
+# installing dependencies
+install.packages(listPackages)
